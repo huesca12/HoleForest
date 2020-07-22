e699d4583bd9827222e176f19ae420348273d3a8
EamonTracey <etracey21@regis.org>
2020-07-22 18:04:39 -0400

    verbose option

--- predict.py
+++ predict.py
@@ -1,34 +1,75 @@
 import click
+from colorama import Fore
 import joblib
 import os
 import pandas
 
+MODEL_PATH = f"{os.path.abspath(os.path.dirname(__file__))}/model/model.joblib"
 PARAM_LIST = ["peakFreq", "snr", "amplitude", "centralFreq", "duration", "bandwidth", "Q-value"]
-PATH = os.path.abspath(os.path.dirname(__file__))
+model = info = warn = success = ...  # All will be modified before use
+
+
+def _info(msg):
+    print(f"[~] {msg}")
+
+
+def _warn(msg):
+    print(f"{Fore.YELLOW}[!] {msg}{Fore.RESET}")
+
+
+def _success(msg):
+    print(f"{Fore.GREEN}[+] {msg}{Fore.RESET}")
 
 
 def run_model(df, output):
+    info("Running model predictions...")
     predictions = model.predict(df)
+    success("Extracted predictions.")
+    info("Running model confidence...")
     probas = ...
-
+    success("Extracted confidence.")
+    info("Initializing output...")
     output_df = df
+    info("Writing output predictions...")
     output_df["prediction"] = predictions
+    info("Writing output confidence...")
     output_df["confidence"] = probas
-    if output is not None:
-        output = output if output.endswith(".csv") else f"{output}.csv"
+    success("Finalized output DataFrame.")
+    if output is None:
+        warn("No output path specified.")
+    else:
+        if not output.endswith(".csv"):
+            warn("Output file does not have the CSV extension.")
+            output = f"{output}.csv"
+            warn(f"Added CSV extension (new output: {output}).")
+        if os.path.exists(output):
+            click.confirm(f"Output path {output} already exists. Do you want to overwrite?", abort=True)
         output_df.to_csv(output, index=False)
+        success(f"Saved output to {output}")
 
 
 @click.group()
-def predict():
-    pass
+@click.option("-v", "--verbose", is_flag=True)
+def predict(verbose):
+    global model, info, warn, success
+    info = _info if verbose else lambda *args, **kwargs: None
+    warn = _warn if verbose else lambda *args, **kwargs: None
+    success = _success if verbose else lambda *args, **kwargs: None
+    info(f"Loading model from {MODEL_PATH}...")
+    model = joblib.load(MODEL_PATH)
+    success(f"Successfully loaded model.")
 
 
 @predict.command()
 @click.argument("file")
 @click.option("--output", "-o")
 def csv(file, output):
+    info(f"Loading {file} into DataFrame...")
+    if not file.endswith(".csv"):
+        warn("Input file does not have the CSV extension.")
     df = pandas.read_csv(file)[PARAM_LIST]
+    success(f"{file} successfully loaded.")
+    info(f"Starting model...")
     run_model(df, output)
 
 
@@ -58,10 +99,17 @@ def glitch(peak_freq, snr, amplitude, central_freq, duration, bandwidth, q_value
         q_value = click.prompt("Q-Value", type=float)
     params = [peak_freq, snr, amplitude, central_freq, duration, bandwidth, q_value]
 
+    info(f"Loading parameters into DataFrame...")
     df = pandas.DataFrame({param: val for (param, val) in zip(PARAM_LIST, params)}, index=[0])
+    success(f"Parameters successfully loaded.")
+    info(f"Starting model...")
     run_model(df, output)
 
 
+@predict.resultcallback()
+def process_result(_, **__):
+    success(f"Finished")
+
+
 if __name__ == "__main__":
-    model = joblib.load(f"{PATH}/model/model.joblib")
     predict()
